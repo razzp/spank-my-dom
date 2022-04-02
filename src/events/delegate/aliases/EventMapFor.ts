7bc55bbc1c912b903f77c6317342581587b0391a
razzp <64423130+razzp@users.noreply.github.com>
2022-04-02 19:19:30 +0100

    Cater for `Element` type

--- src/events/delegate/aliases/EventMapFor.ts
+++ src/events/delegate/aliases/EventMapFor.ts
@@ -4,6 +4,8 @@ type EventMapFor<T extends EventTarget> = T extends Window
     ? DocumentEventMap
     : T extends HTMLElement
     ? HTMLElementEventMap
+    : T extends Element
+    ? ElementEventMap
     : T extends SVGElement
     ? SVGElementEventMap
     : never;
