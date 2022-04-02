0f7684ff2dc68d15e8a303a0d26fc8a22439446b
razzp <64423130+razzp@users.noreply.github.com>
2022-04-02 18:45:15 +0100

    Update exports

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
+export { getClasses } from './classes/getClasses';
+
+export { addEventListener } from './events/addEventListener';
+export { removeEventListener } from './events/removeEventListener';
+
 export { addDelegateEventListener } from './events/delegate/addDelegateEventListener';
 export { removeDelegateEventListener } from './events/delegate/removeDelegateEventListener';
+
+export { createElement } from './manipulation/createElement';
+export { empty } from './manipulation/empty';
+
+export { querySelector } from './retrieval/querySelector';
+export { querySelectorAll } from './retrieval/querySelectorAll';
