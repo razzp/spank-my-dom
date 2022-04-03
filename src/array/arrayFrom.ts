c96e1587e5c4b40266b6697254f3382d50ce8d7f
razzp <64423130+razzp@users.noreply.github.com>
2022-04-03 12:08:48 +0100

    Add annotations

--- src/array/arrayFrom.ts
+++ src/array/arrayFrom.ts
@@ -1,3 +1,8 @@
+/**
+ * Creates an array from an iterable object.
+ * @param iterable An iterable object to convert to an array.
+ * @public
+ */
 function arrayFrom<T>(iterable: Iterable<T> | ArrayLike<T>): T[] {
     return Array.from(iterable);
 }
--- src/classes/getClasses.ts
+++ src/classes/getClasses.ts
@@ -1,12 +1,20 @@
 type SearchType = 'containing' | 'startingwith' | 'endingwith';
 type PatternFactory = (searchString: string) => string;
 
+// This is used to map search types to dynamic RegExp patterns.
 const patternMap = new Map<SearchType, PatternFactory>([
     ['containing', (str) => `[^\\s]+${str}[^\\s]+`],
     ['startingwith', (str) => `${str}[^\\s]+`],
     ['endingwith', (str) => `[^\\s]+${str}`],
 ]);
 
+/**
+ * Get classes from an element that satisfy a predicate search type.
+ * @param searchType The type of search to perform.
+ * @param searchString The string to search for.
+ * @param element The element to perform the search on.
+ * @param ignoreCase Set the case sensitivity of the search.
+ */
 function getClasses(
     searchType: SearchType,
     searchString: string,
@@ -14,6 +22,13 @@ function getClasses(
     ignoreCase?: boolean
 ): string[];
 
+/**
+ * Get classes from a string that satisfy a predicate search type.
+ * @param searchType The type of search to perform.
+ * @param searchString The string to search for.
+ * @param classList The string to perform the search on.
+ * @param ignoreCase Set the case sensitivity of the search.
+ */
 function getClasses(
     searchType: SearchType,
     searchString: string,
@@ -27,16 +42,24 @@ function getClasses(
     context: Element | string,
     ignoreCase = false
 ): string[] {
+    // Get the corresponding pattern from the map.
     const getPattern = patternMap.get(searchType);
 
     if (!getPattern) {
+        // The pattern wasn't found, so throw an error.
         throw new TypeError(`Invalid search type: ${searchType}`);
     }
 
-    const classList = context instanceof Element ? context.className : context;
-    const flags = `g${ignoreCase ? 'i' : ''}`;
-    const matcher = new RegExp(getPattern(searchString), flags);
+    // Build a RegExp for this search.
+    const matcher = new RegExp(
+        getPattern(searchString),
+        `g${ignoreCase ? 'i' : ''}`
+    );
 
+    // Sanitise the input so we have a string.
+    const classList = context instanceof Element ? context.className : context;
+
+    // Run the RegExp and return any matches.
     return classList.match(matcher) ?? [];
 }
 
