10dcbb54b60e00f0d2bb887e4c767d4aaca079f8
razzp <64423130+razzp@users.noreply.github.com>
2022-04-15 00:34:44 +0100

    Add commonjs output

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,20 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import fs from 'fs';
 
+const annotations = fs.readFileSync('docs/annotations.js', 'utf8');
+
 const config = {
     input: 'src/main.ts',
     output: [
         {
+            file: 'dist/main.cjs',
+            format: 'cjs',
+            banner: annotations,
+        },
+        {
             file: 'dist/main.js',
             format: 'esm',
-            banner: fs.readFileSync('docs/annotations.js', 'utf8'),
+            banner: annotations,
         },
     ],
     plugins: [
