1a0cd0191a143002df437ecd720167f81be23287
razzp <64423130+razzp@users.noreply.github.com>
2022-04-26 22:44:35 +0100

    Group exports for better readability

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+// Array
+
 export { arrayFrom } from './array/arrayFrom';
+
+// Attributes
 
 export { addAttribute } from './attributes/addAttribute';
 export { getAttribute } from './attributes/getAttribute';
 export { hasAttribute } from './attributes/hasAttribute';
 export { removeAttribute } from './attributes/removeAttribute';
 export { toggleAttribute } from './attributes/toggleAttribute';
+
+// Classes
 
 export { addClass } from './classes/addClass';
 export { removeClass } from './classes/removeClass';
@@ -14,25 +20,38 @@ export { getClassesEndingWith } from './classes/getClassesEndingWith';
 export { getClassesContaining } from './classes/getClassesContaining';
 export { getClassesStartingWith } from './classes/getClassesStartingWith';
 
+// Events
+
 export { addEventListener } from './events/addEventListener';
 export { removeEventListener } from './events/removeEventListener';
+export { dispatchNewEvent } from './events/dispatchNewEvent';
+
+// Events > Delegate
 
 export { addDelegateEventListener } from './events/delegate/addDelegateEventListener';
 export { removeDelegateEventListener } from './events/delegate/removeDelegateEventListener';
 
-export { dispatchNewEvent } from './events/dispatchNewEvent';
+// Forms
 
 export { serialise } from './forms/serialise';
 
+// Images
+
 export { loadImage } from './images/loadImage';
 export { loadImages } from './images/loadImages';
+
+// Manipulation
 
 export { createElement } from './manipulation/createElement';
 export { emptyElement } from './manipulation/emptyElement';
 export { replaceContents } from './manipulation/replaceContents';
 
+// Retrieval
+
 export { querySelector } from './retrieval/querySelector';
 export { querySelectorAll } from './retrieval/querySelectorAll';
+
+// Traversal
 
 export { closest } from './traversal/closest';
 export { siblings } from './traversal/siblings';
