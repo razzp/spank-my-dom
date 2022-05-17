876dc9db1d8baf75ac81eb7d37d6949678c0a4a6
razzp <64423130+razzp@users.noreply.github.com>
2022-05-17 22:18:39 +0100

    Remove pointless overloads

--- src/classes/classesEndingWith.ts
+++ src/classes/classesEndingWith.ts
@@ -2,43 +2,11 @@ import { escapeRegExp } from './internal/escapeRegExp';
 import { getClasses } from './internal/getClasses';
 
 /**
- * Get classes from an element that end with a specified string.
- * @since 0.3.0
- *
- * @param {string} search The string to search for.
- * @param {Element} element The element to perform the search on.
- * @param {boolean} [ignoreCase] ignoreCase Set the case-sensitivity of the search.
- *
- * @returns {string[]}
- */
-function classesEndingWith(
-    search: string,
-    element: Element,
-    ignoreCase?: boolean
-): string[];
-
-/**
- * Get classes from a string that end with a specified string.
- * @since 0.3.0
- *
- * @param {string} search The string to search for.
- * @param {string} classList The string to perform the search on.
- * @param {boolean} [ignoreCase] ignoreCase Set the case-sensitivity of the search.
- *
- * @returns {string[]}
- */
-function classesEndingWith(
-    search: string,
-    classList: string,
-    ignoreCase?: boolean
-): string[];
-
-/**
  * Get classes from an element or string that end with a specified string.
  * @since 0.3.0
  *
  * @param {string} search The string to search for.
- * @param {(Element|string)} context The context to perform the search on.
+ * @param {Element|string} context The context to perform the search on.
  * @param {boolean} [ignoreCase=false] Set the case-sensitivity of the search.
  *
  * @returns {string[]}
