7ba630c82287ff743129579562b499e0888170c6
razzp <64423130+razzp@users.noreply.github.com>
2024-11-06 16:09:25 +0000

    Add option to include self

--- src/traversal/closest.ts
+++ src/traversal/closest.ts
@@ -4,14 +4,20 @@
  *
  * @param {Element} element The element from which to search.
  * @param {string} selector Selector to match ancestors against.
+ * @param {boolean} [skipSelf=true] Ignore `element` even if it matches.
  *
  * @returns {null|Element}
  */
 function closest<T extends Element>(
     element: Element,
-    selector: string
+    selector: string,
+    skipSelf: boolean = true,
 ): null | T {
-    return element.parentElement?.closest(selector) ?? null;
+    return skipSelf
+        ? (element.parentElement?.closest(selector) ?? null)
+        : element.closest(selector);
 }
 
 export { closest };
+
+// TODO: UPDATE TESTS FOR `skipSelf`
