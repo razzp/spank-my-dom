132ccb968feb4288eb55736b5dadf0dc7ba20881
razzp <64423130+razzp@users.noreply.github.com>
2024-11-28 14:09:25 +0000

    Update annotations

--- src/traversal/closest.ts
+++ src/traversal/closest.ts
@@ -1,9 +1,9 @@
 /**
- * Returns the first ancestor that matches selector. Not inclusive of element.
+ * Returns the first ancestor of `element` that matches selector, or optionally itself.
  * @since 0.2.0
  *
  * @param {Element} element The element from which to search.
- * @param {string} selector Selector to match ancestors against.
+ * @param {string} selector Selector to match against.
  * @param {boolean} [skipSelf=true] Ignore `element` even if it matches.
  *
  * @returns {null|Element}
