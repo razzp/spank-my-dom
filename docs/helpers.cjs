6989937ba16dce9a4182b46598a838b590732334
razzp <64423130+razzp@users.noreply.github.com>
2025-11-03 22:53:39 +0000

    Remove old docs

--- docs/helpers.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-env node, commonjs */
-
-const package = require('../package.json');
-
-module.exports = {
-    description: package.description,
-};
