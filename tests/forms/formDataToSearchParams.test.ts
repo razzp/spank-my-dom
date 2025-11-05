2674bd202800102eb4dc1731066973dbcad84a0c
razzp <64423130+razzp@users.noreply.github.com>
2025-11-05 15:02:49 +0000

    Write tests

--- tests/forms/formDataToSearchParams.test.ts
+++ tests/forms/formDataToSearchParams.test.ts
@@ -1,5 +1,36 @@
-//import { formDataToSearchParams } from '../../src/forms/formDataToSearchParams';
+import { formDataToSearchParams } from '../../src/forms/formDataToSearchParams';
 
-test('TODO', () => {
-    expect(true).toBe(true);
+test('Successfully returns populated URLSearchParams instance', () => {
+    const formData = new FormData();
+
+    formData.append('foo', 'bar');
+
+    const searchParams = formDataToSearchParams(formData);
+
+    expect(searchParams).toBeInstanceOf(URLSearchParams);
+    expect(searchParams.get('foo')).toBe('bar');
+});
+
+test('File is successfully handled using the default handler', () => {
+    const formData = new FormData();
+    const file = new File([], 'bar');
+
+    formData.append('foo', file);
+
+    const searchParams = formDataToSearchParams(formData);
+
+    expect(searchParams).toBeInstanceOf(URLSearchParams);
+    expect(searchParams.get('foo')).toBe('bar');
+});
+
+test('File is successfully handles using a custom handler', () => {
+    const formData = new FormData();
+    const file = new File([], 'bar');
+
+    formData.append('foo', file);
+
+    const searchParams = formDataToSearchParams(formData, () => 'baz');
+
+    expect(searchParams).toBeInstanceOf(URLSearchParams);
+    expect(searchParams.get('foo')).toBe('baz');
 });
--- tests/forms/getFormData.test.ts
+++ tests/forms/getFormData.test.ts
@@ -1,5 +1,39 @@
-//import { getFormData } from '../../src/forms/getFormData';
+/**
+ * @jest-environment jsdom
+ */
 
-test('TODO', () => {
-    expect(true).toBe(true);
+import { assertIsNotNull } from 'bossy-boots';
+import { getFormData } from '../../src/forms/getFormData';
+
+beforeAll(() => {
+    document.body.innerHTML = `
+        <form class="target">
+            <input type="hidden" name="foo" value="bar">
+        </div>
+    `;
+});
+
+test('Successfully returns a populated FormData instance', () => {
+    const target = document.querySelector<HTMLFormElement>('.target');
+
+    assertIsNotNull(target);
+
+    const data = getFormData(target);
+
+    expect(data).toBeInstanceOf(FormData);
+    expect(data.get('foo')).toBe('bar');
+});
+
+test('Successfully returns a populated FormData instance with additional entries', () => {
+    const target = document.querySelector<HTMLFormElement>('.target');
+
+    assertIsNotNull(target);
+
+    const data = getFormData(target, {
+        baz: 'qux',
+    });
+
+    expect(data).toBeInstanceOf(FormData);
+    expect(data.get('foo')).toBe('bar');
+    expect(data.get('baz')).toBe('qux');
 });
