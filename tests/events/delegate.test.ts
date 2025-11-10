f2e87e2b86d47f9218ba607e6bb4114f5d7aa22f
razzp <64423130+razzp@users.noreply.github.com>
2025-11-10 17:32:31 +0000

    Write tests

--- tests/events/delegate.test.ts
+++ tests/events/delegate.test.ts
@@ -1,5 +1,68 @@
-//import { delegate } from '../../src/events/delegate';
+/**
+ * @jest-environment jsdom
+ */
 
-test('TODO', () => {
-    expect(true).toBe(true);
+import { assertIsNotNull } from 'bossy-boots';
+import { type DelegateEvent, delegate } from '../../src/events/delegate';
+
+beforeAll(() => {
+    document.body.innerHTML = `
+        <div class="parent target">
+            <div class="child target">
+                <div class="grandchild target"></div>
+            </div>
+        </div>
+    `;
+});
+
+test('Callback is successfully fired on matched target', () => {
+    const callback = jest.fn<unknown, DelegateEvent<Event>[]>();
+    const parent = document.querySelector('.parent');
+    const grandchild = document.querySelector('.grandchild');
+
+    assertIsNotNull(parent);
+    assertIsNotNull(grandchild);
+
+    document.addEventListener('click', delegate('.parent', callback));
+
+    grandchild.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    const delegateEvent = callback.mock.calls[0][0];
+
+    expect(delegateEvent.delegateTarget).toBe(parent);
+    expect(delegateEvent.event.target).toBe(grandchild);
+});
+
+test('Callback is repeatedly called as event bubbles and targets match', () => {
+    const callback = jest.fn();
+    const grandchild = document.querySelector('.grandchild');
+
+    assertIsNotNull(grandchild);
+
+    document.addEventListener('click', delegate('.target', callback));
+
+    grandchild.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+    expect(callback).toHaveBeenCalledTimes(3);
+});
+
+test('Callback is ignored on subsequent matches after delegation is forced to stop', () => {
+    const callback = jest.fn();
+    const grandchild = document.querySelector('.grandchild');
+
+    assertIsNotNull(grandchild);
+
+    document.addEventListener(
+        'click',
+        delegate('.target', (event) => {
+            event.stopDelegation();
+            callback(event);
+        }),
+    );
+
+    grandchild.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+
+    expect(callback).toHaveBeenCalledTimes(1);
 });
--- tests/manipulation/createElement.test.ts
+++ tests/manipulation/createElement.test.ts
@@ -2,8 +2,44 @@
  * @jest-environment jsdom
  */
 
-//import { createElement } from '../../src/manipulation/createElement';
+import { createElement } from '../../src/manipulation/createElement';
 
-test('TODO', () => {
-    expect(true).toBe(true);
+test('Basic element is successfully created', () => {
+    const element = createElement('div');
+
+    expect(element).toBeInstanceOf(HTMLDivElement);
+});
+
+test('Element with options specified is successfully created', () => {
+    const nodeToPrepend = document.createElement('span');
+    const nodeToAppend = document.createElement('span');
+
+    const element = createElement('div', {
+        content: 'foo',
+        attributes: {
+            ariaExpanded: 'true',
+        },
+        classes: ['bar', 'baz'],
+        styles: {
+            color: 'rebeccapurple',
+        },
+        data: {
+            qux: 'quux',
+        },
+        prepend: [nodeToPrepend],
+        append: [nodeToAppend],
+    });
+
+    const childNodes = [...element.childNodes];
+
+    expect(element).toBeInstanceOf(HTMLDivElement);
+    expect(element.getAttribute('aria-expanded')).toBe('true');
+    expect(element.style.color).toBe('rebeccapurple');
+    expect(element.className).toBe('bar baz');
+    expect(element.dataset.qux).toBe('quux');
+
+    expect(childNodes[0]).toBe(nodeToPrepend);
+    expect(childNodes[1].nodeType).toBe(Node.TEXT_NODE);
+    expect(childNodes[1].textContent).toBe('foo');
+    expect(childNodes[2]).toBe(nodeToAppend);
 });
